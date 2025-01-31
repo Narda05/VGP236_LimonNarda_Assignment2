bc2b35b5fa0f2be2716190496dac89ab00d078be
Narda05 <153568349+Narda05@users.noreply.github.com>
2025-01-30 19:16:05 -0800

    Collectables
    The amount of colectables that have in the leves you need to recollect to pas in the next level using the scrip level Control and add in the score required the  total colectables that you need to have

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,13 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    private UnityEvent onScoreAdded = new UnityEvent();
 
     void Awake() 
     {
@@ -24,10 +27,20 @@ public class GameManager : MonoBehaviour
     {
         score += amount;
         scoreText.text = "-  " + score.ToString();
+        onScoreAdded.Invoke();
     }
 
     public int GetScore()
     {
         return score;
+    }
+
+    public void AddScoreAddedListener(UnityAction action)
+    {
+        onScoreAdded.AddListener(action);
+    }
+    public void RemoveScoreAddedListener(UnityAction action)
+    {
+        onScoreAdded.RemoveListener(action);
     }
 }
--- Assets/Scripts/LevelControl.cs
+++ Assets/Scripts/LevelControl.cs
@@ -1,9 +1,28 @@
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
 
 public class LevelControl : MonoBehaviour
 {
     public int sceneBuildIndex;
+    public int scoreRequired = 0;
+
+    public Collider2D doorCollider = null;
+
+    private void Start()
+    {
+        doorCollider = GetComponent<Collider2D>();
+        doorCollider.enabled = false;
+        GameManager.instance.AddScoreAddedListener(OnScoreAdded);
+    }
+
+    private void OnScoreAdded()
+    {
+        int score = GameManager.instance.GetScore();
+        // door closed = score < scoreRequired
+        // door open = score >= scoreRequired
+        doorCollider.enabled = score >= scoreRequired;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
